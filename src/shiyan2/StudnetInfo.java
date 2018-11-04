f830265556c317131a77ba54917e353fb788efb7
zhengzhizhong <1549308440@qq.com>
2018-11-04 22:05:46 +0800

    aaa啊啊

---
+++ src/shiyan2/StudnetInfo.java
@@ -0,0 +1,101 @@
+package shiyan2;
+
+public class StudnetInfo {
+
+	private String id;
+
+    private String name;
+
+    private String classes;
+
+    private String place;
+    private String data;
+    private String sex;
+    private String age;
+    private String tel;
+    private String zhuanye;
+    private String yuanxi;
+    
+    public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getZhuanye() {
+		return zhuanye;
+	}
+
+	public void setZhuanye(String zhuanye) {
+		this.zhuanye = zhuanye;
+	}
+
+	public String getYuanxi() {
+		return yuanxi;
+	}
+
+	public void setYuanxi(String yuanxi) {
+		this.yuanxi = yuanxi;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getClasses() {
+		return classes;
+	}
+
+	public void setClasses(String classes) {
+		this.classes = classes;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+
+
+}
